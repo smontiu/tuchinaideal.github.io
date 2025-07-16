859a7a9b2f372ac2674afbf2fea5b82bd45da0b7
smontiu <montiu.torguet.santiago@gmail.com>
2025-07-16 23:39:40 +0200

    form response

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const {setGlobalOptions} = require('firebase-functions');
 const { onRequest } = require('firebase-functions/v2/https');
 const logger = require('firebase-functions/logger');
+const path = require('node:path');
 
 const {initializeApp} = require('firebase-admin/app');
 const {getFirestore} = require('firebase-admin/firestore');
@@ -9,17 +10,18 @@ setGlobalOptions({ maxInstances: 1 });
 initializeApp();
 
 exports.addInitialContact = onRequest({ cors: ['tuchinaideal.com'] }, async (req, res) => {
-   const { email, duration, fullName, nationality, plan } = req.body;
-   const writeResult = await getFirestore()
-        .collection('leads')
-        .add({
-          email,
-          duration,
-          fullName,
-          nationality,
-          plan
-        });
+   const { email, duration = 0, fullName = '', nationality = '', plan = 1 } = req.body;
+   await getFirestore()
+      .collection('leads')
+      .add({
+        email,
+        duration,
+        fullName,
+        nationality,
+        plan,
+        createdAt: +new Date()
+      });
     
     logger.info('[addInitialContact] lead added', { structuredData: true });
-    res.status(200).json({result: `Message with ID: ${writeResult.id} lead added.`});
+    res.status(200).type('html').sendFile(path.join(__dirname, 'thanks.html'));
 });
