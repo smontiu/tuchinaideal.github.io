f88195f1f29e1dd5ed9fb37cb733ccf0a37ea145
smontiu <montiu.torguet.santiago@gmail.com>
2025-07-17 22:31:31 +0200

    add thanks page and redirection

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const {setGlobalOptions} = require('firebase-functions');
 const { onRequest } = require('firebase-functions/v2/https');
 const logger = require('firebase-functions/logger');
-const path = require('node:path');
+const sgMail = require('@sendgrid/mail');
 
 const {initializeApp} = require('firebase-admin/app');
 const {getFirestore} = require('firebase-admin/firestore');
@@ -9,8 +9,11 @@ const {getFirestore} = require('firebase-admin/firestore');
 setGlobalOptions({ maxInstances: 1 });
 initializeApp();
 
+sgMail.setApiKey(process.env.SENDGRID);
+
 exports.addInitialContact = onRequest({ cors: ['tuchinaideal.com'] }, async (req, res) => {
-   const { email, duration = 0, fullName = '', nationality = '', plan = 1 } = req.body;
+  try {
+    const { email = '', duration = 0, fullName = '', nationality = '', plan = 1 } = req.body;
    await getFirestore()
       .collection('leads')
       .add({
@@ -22,6 +25,20 @@ exports.addInitialContact = onRequest({ cors: ['tuchinaideal.com'] }, async (req
         createdAt: +new Date()
       });
     
+    const msg = {
+      to: email,
+      from: 'tuchinaideal@gmail.com',
+      subject: 'Test tuchinaideal',
+      text: 'test content',
+      html: '<strong>html test content</strong>',
+    };
+    if (email) {
+      await sgMail.send(msg);
+    }
     logger.info('[addInitialContact] lead added', { structuredData: true });
-    res.status(200).type('html').sendFile(path.join(__dirname, 'thanks.html'));
+    res.redirect('https://tuchinaideal.com/thanks')
+  } catch(err) {
+    logger.error('[addInitialContact] lead err', { err, structuredData: true });
+    res.status(500).send();
+  }
 });
